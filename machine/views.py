e460f151f962e8fbd31c3416806bdeed53bd0c19
jindalpawan <jindalpawan99@gmail.com>
2020-06-10 00:52:20 +0530

    update view

--- machine/views.py
+++ machine/views.py
@@ -26,20 +26,22 @@ AtmAmount=0  # current amount in the ATM
 
 # Deposite amount in the Account
 class Deposite(APIView):	
-	permission_classes = (IsAuthenticated,)
+	permission_classes = (IsAuthenticated,) 
 	def get(self, request):
+		#only authenticate user can access
 		data=json.dumps(request.data)
 		data=json.loads(data)
 		amount=data["amount"]
 		if amount%100==0:
 			user=request.user
+			#deposite money in the account and ATM
 			user.balance=user.balance+amount
 			AtmAmount=AtmAmount+amount
-			n2000=n2000+data["n2000"]
+			n2000=n2000+data["n2000"]  #update notes in the ATM
 			n500=n500+data["n500"]
 			n200=n200+data["n200"]
 			n100=n100+data["n100"]
-			dic={'Error':"success"}
+			dic={'success':"success"}
 		else:
 			dic={'Error':"amount should be multiple by 100"}
 		return Response(dic)
@@ -57,35 +59,60 @@ class Withdraw(APIView):
 		if amount>20000:
 			dic["Error"]="Not enough cash in your account"
 		else:
-			if user.balance>=amount:
-				if AtmAmount>=amount:
-					if amount%100==0:
+			if user.balance>=amount: # check amount in the Account or not
+				if AtmAmount>=amount:  # check amount in the ATM or not
+					if amount%100==0: # check amount multiple of 100 or not
 						user.balance=user.balance- amount
-						
-						#couunt how many notes of 20000 will require
+						AtmAmount=AtmAmount-amount
+
+						#if 2000 notes in the ATM then withdraw
 						if n2000:
 							x=amount/2000
-							n2000=n2000-x
-							amount=amount%2000
-							dic["n2000"]=x
+							if n2000>=x:
+								n2000=n2000-x
+								amount=amount%2000
+								dic["n2000"]=x
+							else:
+								amount=amount-(2000*n2000)
+								dic["n2000"]=n2000
+								n2000=0
 
-						if amount>=(500*n500):
+						#if 500 notes in the ATM then withdraw
+						if n500:
 							x=amount/500
-							n500=n500-x
-							amount=amount%500
-							dic["n500"]=x
+							if n500>=x:
+								n500=n500-x
+								amount=amount%500
+								dic["n500"]=x
+							else:
+								amount=amount-(500*n500)
+								dic["n500"]=n500
+								n500=0
 
-						if amount>=(200*n200):
+						#if 200 notes in the ATM then withdraw
+						if n200:
 							x=amount/200
-							n200=n200-x
-							amount=amount%200
-							dic["n200"]=x
+							if n200>=x:
+								n200=n200-x
+								amount=amount%200
+								dic["n200"]=x
+							else:
+								amount=amount-(200*n200)
+								dic["n200"]=n200
+								n200=0
 
-						if amount>=(100*n100):
+						#if 100 notes in the ATM then withdraw
+						if n100:
 							x=amount/100
-							n100=n100-x
-							amount=amount%100
-							dic["n100"]=x
+							if n100>=x:
+								n100=n100-x
+								amount=amount%100
+								dic["n100"]=x
+							else:
+								amount=amount-(100*n100)
+								dic["n100"]=n100
+								n100=0
+
 					else:
 						dic["Error"]="Amount shuold be multiple of Rs100"
 				else:
@@ -106,12 +133,12 @@ class AuthViewSet(viewsets.ViewSet):
 		data=json.loads(data)
 		userlength=len(data["username"])
 		passlength=len(data["password"])
-		if userlength==8 and passlength==4:
-			user = get_and_authenticate_user(data["username"], data["password"])
+		if userlength==8 and passlength==4: #check digit count in username(user's card) and pin
+			user = get_and_authenticate_user(data["username"], data["password"])  #check input is valid or not
 			data = serializers.AuthUserSerializer(user).data
 			return Response(data=data, status=status.HTTP_200_OK)
 		else:
-			dic={'Error':"username and password not matched"}
+			dic={'Error':"Input valid digites"}
 			return Response(dic)
 
 	@action(methods=['GET', ], detail=False)
